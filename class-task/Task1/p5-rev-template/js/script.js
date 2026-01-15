e65dc37c8127f66d36ac864aac9f4963c6ef3183
bbwinnie <nini200702@gmail.com>
2026-01-15 15:41:26 -0500

    qu3 finished

--- class-task/Task1/p5-rev-template/js/script.js
+++ class-task/Task1/p5-rev-template/js/script.js
@@ -2,38 +2,38 @@
 
 // working with xueyi xia
 
-//set the size, color, position for circle One.
-let circleOne = {
-    x: 50,
-    y: 50,
-    width: 50,
-    height: 50,
-    r: 255,
-    g: 255,
-    b: 255,
-}
+// //set the size, color, position for circle One.
+// let circleOne = {
+//     x: 50,
+//     y: 50,
+//     width: 50,
+//     height: 50,
+//     r: 255,
+//     g: 255,
+//     b: 255,
+// }
 
-//set the size, color, position for circle Two.
-let circleTwo = {
-    x: 130,
-    y: 130,
-    width: 70,
-    height: 70,
-    r: 250,
-    g: 200,
-    b: 210,
-}
+// //set the size, color, position for circle Two.
+// let circleTwo = {
+//     x: 130,
+//     y: 130,
+//     width: 70,
+//     height: 70,
+//     r: 250,
+//     g: 200,
+//     b: 210,
+// }
 
-//set the size, color, position for circle Three.
-let circleThree = {
-    x: 200,
-    y: 200,
-    width: 100,
-    height: 100,
-    r: 200,
-    g: 200,
-    b: 210,
-}
+// //set the size, color, position for circle Three.
+// let circleThree = {
+//     x: 200,
+//     y: 200,
+//     width: 100,
+//     height: 100,
+//     r: 200,
+//     g: 200,
+//     b: 210,
+// }
 
 //draw the canvas and background color
 function setup() {
@@ -47,11 +47,11 @@ function setup() {
 // show three circle
 function draw() {
 
-    drawEllipse(circleOne.x, circleOne.y, circleOne.width, circleOne.height, circleOne.r, circleOne.g, circleOne.b);
-    drawEllipse(circleTwo.x, circleTwo.y, circleTwo.width, circleTwo.height, circleTwo.r, circleTwo.g, circleTwo.b);
-    drawEllipse(circleThree.x, circleThree.y, circleThree.width, circleThree.height, circleThree.r, circleThree.g, circleThree.b)
-    // drawC1();
-    // drawC2();
+    // drawEllipse(circleOne.x, circleOne.y, circleOne.width, circleOne.height, circleOne.r, circleOne.g, circleOne.b);
+    // drawEllipse(circleTwo.x, circleTwo.y, circleTwo.width, circleTwo.height, circleTwo.r, circleTwo.g, circleTwo.b);
+    // drawEllipse(circleThree.x, circleThree.y, circleThree.width, circleThree.height, circleThree.r, circleThree.g, circleThree.b)
+    // // drawC1();
+    // // drawC2();
     // drawC3();
 
 }
@@ -84,12 +84,14 @@ function draw() {
 // }
 
 
-// QU2 
+// QU2 draw three Ellipse
+// function drawEllipse(x, y, w, h, r, g, b) {
+//     push();
+//     noStroke();
+//     fill(r, g, b);
+//     ellipse(x, y, w, h);
+//     pop();
+// }
 
-function drawEllipse(x, y, w, h, r, g, b) {
-    push();
-    noStroke();
-    fill(r, g, b);
-    ellipse(x, y, w, h);
-    pop();
-}
+//QU3 draw three square 
+
