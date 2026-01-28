2cb0c1fe6ca5c8a81a5909c8ce7ab33dca389693
bbwinnie <nini200702@gmail.com>
2026-01-28 14:50:33 -0500

    tesk 3 finished

--- class-task/Task2/CART263-TASK-2/task-2-js/script.js
+++ class-task/Task2/CART263-TASK-2/task-2-js/script.js
@@ -205,7 +205,7 @@ function setup() {
     // document.getElementById("3").innerHTML = "<h2>TEST 123</h2>";
 
     /*************************************** */
-    /* 5: Select the fourth paragraph element on the page and 
+    /* 5: Select the fourth paragraph element on the page and
     add to the existing content an h2 element containing the text `TEST 123`
     /***CODE */
     // document.getElementById("4").innerHTML += "<h2>TEST 123</h2>";
@@ -220,7 +220,7 @@ function setup() {
 
 
     /*************************************** */
-    /* 7: Add the following array variable: let colors = ['red','blue','green','orange'];, 
+    /* 7: Add the following array variable: let colors = ['red','blue','green','orange'];,
     then access all elements with class name inner-container and save to a variable called `innerContainers`. 
     Next, iterate over the colors array, and for each color: 
     assign the element from innerContainers variable with the same index 
@@ -229,10 +229,11 @@ function setup() {
     /***CODE */
     // let colors = ['red', 'blue', 'green', 'orange'];
 
-    // for (i = 0; i <= 4; i++) {
+    // for (i = 0; i <= 3; i++) {
     //     document.getElementsByClassName("inner-container")[i].style.background = colors[i];
     // }
 
+    //please see the image of task-2-reference-image/tesk_1.png
     /*************************************** */
     /*** END PART TWO MODIFY */
 
@@ -248,26 +249,46 @@ function setup() {
     /* 1E:  Set the background of this paragraph element to be green */
     /* 1F:  Set the color of the text in this paragraph element to be white */
     /* 1G: Append this new element to the parent variable within the function. */
-    /* 1H: Iterate through the allPTagsThree array and call customCreateElement(), 
+    /* 1H: Iterate through the allPTagsThree array and call customCreateElement(),
     passing the current allPTagsThree element as the parent with each iteration.*/
     /***CODE */
+    // let allPTagsThree = document.querySelectorAll("p");
+
+    // function customCreateElement(parent) {
+    //new element
+    //     let newPra = document.createElement("p");
+    //     newPra.innerHTML = "using create Element";
+    //     newPra.style.backgroundColor = "green";
+    //     newPra.style.color = "white";
+    // access parent element
+    //     parent.appendChild(newPra);
+    // };
+
+    //for each paragraph element, will add the newPra to the orginal paragraph.
+    // for (let i = 0; i < allPTagsThree.length; i++) {
+    //     customCreateElement(allPTagsThree[i]);
+    // }
+
 
 
     /***EXPLANATION::
+     * This code selects all paragraph elements, then uses a loop and a custom function to dynamically create 
+     * and append a new styled paragraph inside each existing paragraph element.
      * 
+     * please see the image of task-2-reference-image/tesk_3_1.png
      * 
      */
 
     /*************************************** */
     /* 2: GRID OF BOXES */
     /* 2A: Create another new function: function customNewBoxCreate(parent){ //body }*/
-    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv. 
+    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv.
     /* 2C:Then append this new element to the parent variable within the function. 
     /* 2D:Finally, return</code> this new element */
-    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)). 
+    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)).
         Call the customNewBoxCreate function, in order to generate a new div -> representing each cell in the grid. 
         Ensure that the parent element for each of these new divs is the element whose id is named `new-grid`*/
-    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense... 
+    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense...
         Fix this by doing the following: every time you call customNewBoxCreate() - save the current returned element 
         in a variable i.e. returnedDiv. 
         Set the style (left and top) to the of this element to 
@@ -280,32 +301,123 @@ function setup() {
 
     /***CODE */
 
+    function customNewBoxCreate(parent) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("testDiv");
+        parent.appendChild(newDiv);
+        return newDiv;
+    }
+
+    let gridParent = document.getElementById("new-grid");
+    let cellSize = 40;
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let returnedDiv = customNewBoxCreate(gridParent);
+            let x = i * cellSize;
+            let y = j * cellSize;
+            returnedDiv.style.left = x + "px";
+            returnedDiv.style.top = y + "px";
+            returnedDiv.style.width = cellSize + "px";
+            returnedDiv.style.height = cellSize + "px";
+            returnedDiv.style.background = "cornflowerblue";
+            returnedDiv.textContent = `odd`;
+            if (j % 2 === 0) {
+                returnedDiv.style.background = "white";
+                returnedDiv.textContent = `even`;
+            }
+
+        }
+    }
+
+    console.log(document.getElementsByClassName("testDiv"));
+
 
     /***EXPLANATION::
+     * HTMLCollection(100) [div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv,
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv,
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv,
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, 
+     * div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv, div.testDiv]
      * 
+     * 
+     * 
+     * when selecting all the element name with "testDiv". the console.log return 100 elements. it is becuase the for
+     * loop that run 10 times for rows and 10 times for columns. Each times of the inner loop that creates a new
+     * element and assigns it the class testDiv. As a result, a total of 10x10=100div elements with the class
+     * name testDiv are added to the DOM.
+     * 
+     * please see the image of task-2-reference-image/tesk_3.png
      * 
      */
 
     /*************************************** */
     /* 3: GRID OF BOXES II */
 
-    /* 3A: Create ANOTHER nested for loop - in order to generate a new grid ... 
+    /* 3A: Create ANOTHER nested for loop - in order to generate a new grid ...
         USE the same customNewBoxCreate function..., the only difference is that the parent element 
         for each of these new divs is the element whose id is `new-grid-three`. */
-    /* 3B: Then: write the code to check when a column is a multiple of 3 (no remainder), 
+    /* 3B: Then: write the code to check when a column is a multiple of 3 (no remainder),
         when it is a column where the remainder is 1 or when the remainder is 2 ... 
         HINT:: look up the % operator.. */
-    /* 3C: Then for each of the above cases: give the new divs in the first case a background of red, 
+    /* 3C: Then for each of the above cases: give the new divs in the first case a background of red,
             then the second a background of orange and the third yellow. */
-    /*  3D: Finally, let each div contain the text content representing the associated remainder 
+    /*  3D: Finally, let each div contain the text content representing the associated remainder
         when dividing by three. */
 
     /***CODE */
 
+    function customNewBoxCreate(parent) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("testDiv");
+        parent.appendChild(newDiv);
+        return newDiv;
+    }
+
+    let grid2Parent = document.getElementById("new-grid-three");
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let returnedDiv = customNewBoxCreate(grid2Parent);
+            let x = i * cellSize;
+            let y = j * cellSize;
+            returnedDiv.style.left = x + "px";
+            returnedDiv.style.top = y + "px";
+            returnedDiv.style.width = cellSize + "px";
+            returnedDiv.style.height = cellSize + "px";
+
+            if (i % 3 === 0) {
+                returnedDiv.style.background = "red";
+                returnedDiv.textContent = `0`;
+            }
+            else if (i % 3 === 1) {
+                returnedDiv.style.background = "orange";
+                returnedDiv.textContent = `1`;
+            }
+            else if (i % 3 === 2) {
+                returnedDiv.style.background = "yellow";
+                returnedDiv.textContent = `2`;
+            }
+
+        }
+    }
+
 
     /***EXPLANATION::
+     * This is using the same way as part two, adding the grid to the DOM. 
+     *  this time we need to change the color by column. which we can using the % operator. i/3 reminder 0 will be the first column, 
+     * reminder 1 will be secound column,reminder 2 will be third column. after that we can dirct changing the color by using stlye. background,
+     * and adding the text by using the textContent.
      * 
-     * 
+     * please see the image of task-2-reference-image/tesk_3.png
      */
 
     /*************************************** */
